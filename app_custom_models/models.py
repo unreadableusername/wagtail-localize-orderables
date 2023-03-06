b2bc06031ffcfab5d2b810693eb22775b34a17b2
Christoph Petermann <cpetermann@geomar.de>
2023-03-06 12:53:37 +0100

    add cutsom model for orderable

---
+++ app_custom_models/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+from wagtail.admin.panels import FieldPanel, FieldRowPanel
+from wagtail.snippets.models import register_snippet
+
+from wagtail.search import index
+from wagtail.models import TranslatableMixin
+
+
+@register_snippet
+class ContactPerson(index.Indexed, TranslatableMixin, models.Model):
+    first_name = models.CharField(max_length=255, blank=False, null=False)
+    last_name = models.CharField(max_length=255, blank=False, null=False)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    further_information = models.TextField(blank=True, null=True)
+
+    panels = [
+        FieldPanel('image', heading='Profilbild'),
+        FieldRowPanel(
+            [
+                FieldPanel('first_name', heading='Vorname'), 
+                FieldPanel('last_name', heading='Nachname')
+            ], 
+            classname='name'
+        ),
+        FieldPanel('further_information')
+    ]
+
+    search_fields = [
+        # Filters in SnippetChooser
+        index.FilterField('locale_id'),
+        index.FilterField('last_name', partial_match=True),
+        index.FilterField('first_name', partial_match=True),
+
+        # Search in WagtailAdmin Snippets
+        index.AutocompleteField('last_name'),
+        index.AutocompleteField('first_name'),
+    ]
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta(TranslatableMixin.Meta):     # noqa
+        verbose_name = "Ansprechperson"
+        verbose_name_plural = "Anprechpersonen"
