f2aa6c7990418e2922fe0c92cdda66d08b5fa940
Christoph Petermann <cpetermann@geomar.de>
2023-03-06 13:24:05 +0100

    display model on home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from wagtail.admin.panels import InlinePanel
 
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
+from modelcluster.fields import ParentalKey
+
+class ContactPersonOrderable(Orderable, models.Model):
+    page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='contact_persons')
+    person = models.ForeignKey('app_custom_models.ContactPerson', on_delete=models.CASCADE, related_name='+')
 
 
 class HomePage(Page):
-    pass
+
+    content_panels = Page.content_panels + [
+        InlinePanel('contact_persons', label="Kontaktpersonen"),
+    ]
