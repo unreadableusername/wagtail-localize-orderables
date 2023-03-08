fc99eea7f2a96ba08c43c51f5061c483ac3b7f66
Christoph Petermann <cpetermann@geomar.de>
2023-03-08 12:50:15 +0100

    Made model translatable and updated README.

--- home/models.py
+++ home/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from wagtail.admin.panels import InlinePanel
 
 from wagtail.models import Page, Orderable
+from wagtail.models import TranslatableMixin
 from modelcluster.fields import ParentalKey
 
-class ContactPersonOrderable(Orderable, models.Model):
+class ContactPersonOrderable(TranslatableMixin, Orderable):
     page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='contact_persons')
     person = models.ForeignKey('app_custom_models.ContactPerson', on_delete=models.CASCADE, related_name='+')
 
+    class Meta(TranslatableMixin.Meta ,Orderable.Meta):
+        pass
 
 class HomePage(Page):
 
